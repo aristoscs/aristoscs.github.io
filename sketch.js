d82ea5b4e27e66a861ed0b2dde179a6745a7af41
ageorg08 <aristoscs@gmail.com>
2023-08-02 16:02:20 +0300

    Added PriorityQueue

--- sketch.js
+++ sketch.js
@@ -1,13 +1,114 @@
+class MaxHeap {
+	
+  constructor() {
+    this.heap = [];
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heap.length;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heap.length;
+  }
+
+  hasParent(index) {
+    return this.getParentIndex(index) >= 0;
+  }
+
+  leftChild(index) {
+    return this.heap[this.getLeftChildIndex(index)];
+  }
+
+  rightChild(index) {
+    return this.heap[this.getRightChildIndex(index)];
+  }
+
+  parent(index) {
+    return this.heap[this.getParentIndex(index)];
+  }
+
+  swap(indexOne, indexTwo) {
+    const temp = this.heap[indexOne];
+    this.heap[indexOne] = this.heap[indexTwo];
+    this.heap[indexTwo] = temp;
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    return this.heap[0];
+  }
+
+  remove() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    const item = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.heapifyDown();
+    return item;
+  }
+
+  add(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.hasParent(index) && this.parent(index).getG() < this.heap[index].getG()) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+      if (this.hasRightChild(index) && this.rightChild(index).getG() > this.leftChild(index).getG()) {
+        largerChildIndex = this.getRightChildIndex(index);
+      }
+      if (this.heap[index].getG() > this.heap[largerChildIndex].getG()) {
+        break;
+      } else {
+        this.swap(index, largerChildIndex);
+      }
+      index = largerChildIndex;
+    }
+  }
+}
+
+
 class State {
     
 	constructor(parent, direction, g, hash, pi, pj) {
         this.parent = parent;
         this.direction = direction;
         this.g = g;
+		this.h = 0;
         this.hash = hash;
         this.pi = pi;
         this.pj = pj;
     }
+	
+	getF() {
+		return this.g + this.h;
+	}
 
 }
 
