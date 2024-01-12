0a0b168d4921cb8cc55fe5949752cc0d3722df2a
ageorg08 <aristoscs@gmail.com>
2024-01-12 12:39:37 +0200

    Add

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, 0);
-    const minutes = now.getMinutes().toString().padStart(2, 0);
-    const seconds = now.getSeconds().toString().padStart(2, 0);
-    const timeString = `${hours}:${minutes}:${seconds}`
-    document.getElementById("clock").textContent = timeString;
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
 }
 
-updateClock();
-setInterval(updateClock, 1000);
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+    
+}
